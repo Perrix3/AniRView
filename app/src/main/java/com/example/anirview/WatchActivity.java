6e0525bcbeb9e9c0327e911171217b0408adca1a
Perrix3 <perrix@klatul.com>
2024-11-22 11:19:23 +0100

    Linked streaming services in where to watch anime.

--- app/src/main/java/com/example/anirview/WatchActivity.java
+++ app/src/main/java/com/example/anirview/WatchActivity.java
@@ -1,9 +1,13 @@
 package com.example.anirview;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,9 +31,29 @@ public class WatchActivity extends AppCompatActivity {
             }
         });
 
+        TextView crunchy=findViewById(R.id.crunchyrollLink);
+        crunchy.setOnClickListener(v->{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.crunchyroll.com/"));
+            startActivity(browserIntent);
+        });
 
+        TextView netflix=findViewById(R.id.netflixLink);
+        netflix.setOnClickListener(v->{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.netflix.com/"));
+            startActivity(browserIntent);
+        });
 
+        TextView amazon=findViewById(R.id.amazonPrimeLink);
+        amazon.setOnClickListener(v->{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.disneyplus.com/"));
+            startActivity(browserIntent);
+        });
 
+        TextView disney=findViewById(R.id.disneyPlusLink);
+        disney.setOnClickListener(v->{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.com/gp/video/storefront/"));
+            startActivity(browserIntent);
+        });
 
     }
 }
